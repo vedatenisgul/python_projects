71ab75c2bec536c7925b8455596e63c882fc0369
vedatenisgul <gulv22@itu.edu.tr>
2024-08-05 17:19:08 +0300

    added error handling

--- projects/day_26/main.py
+++ projects/day_26/main.py
@@ -1,7 +1,19 @@
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
 alphabet = {row.letter: row.code for (index, row) in data.iterrows()}
-user_input = input("Enter a word: ").upper()
-result = [alphabet[letter] for letter in user_input]
-print(result)
+
+
+def generate_phonetic():
+    user_input = input("Enter a word: ").upper()
+    try:
+        result = [alphabet[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(result)
+
+
+generate_phonetic()
